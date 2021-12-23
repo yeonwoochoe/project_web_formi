716a91aea04d0785ac5c8798a8283ca07301144a
YEONWOO <89890240+yeonwoochoe@users.noreply.github.com>
2021-12-23 23:35:57 +0900

    [feat] formi 🆕 viewBox 광고 슬라이드 js 구현 작업 진행중

---
+++ code/js/src/viewbox_slide.js
@@ -0,0 +1,99 @@
+//viewbox_slide.js
+
+// 시나리오
+// 다음버튼 클릭시 - 'fadeOut'/(fadeIn) 기능을 이용하여, 이미지 변경
+// 1. 현재 보이는 이미지(z-index:100) 에 버튼 클릭시 보이는 이미지 바로뒤 에 이미지가 나타나게 만들기
+// 2. 현재 보이는 이미지를 투명도 1->0으로 변경 ( 100%, 50%, 0 )
+// 3. 투명도가 0이 되면 display:none으로 사라지게
+// 4. animation기법으로 처리되어 사라진 이미지 style(html에 새겨진) 제거
+// -------------------------------------
+
+
+(() => {
+  // 선택자 변수
+  const elViewBox = document.querySelector('#viewBox');
+  const elSlideWrap = elViewBox.querySelector('.slide_wrapper');
+  
+  const elBtnPart = elSlideWrap.querySelector('.view_btn');
+  const elNext = elBtnPart.querySelector('.next');
+  const elPrev = elBtnPart.querySelector('.prev');
+  
+  const indi = elSlideWrap.querySelector('.indicator'); // 인디케이터 사용 하지 않도록 제거
+  // indi.remove();
+  const indiLi = indi.querySelectorAll('li');
+  
+  const slideConWrap = elSlideWrap.querySelector('.slide_con_wrap');
+  const slideDiv  = slideConWrap.querySelectorAll('div');
+  
+  // 옵션변수
+  let [CLASS_CHECK, countIndex, timed, cssFn, permission] = ['act', 0, 500, 'ease', true];
+  let beforeIndex = countIndex;
+  const divLen = slideDiv.length;
+  // -------------------------------------------------------------
+  slideDiv[countIndex].classList.replace('on', CLASS_CHECK); // 기존 li class이름 변경
+  indiLi[countIndex].classList.add('act');
+  // -------------------------------------------------------------
+  // 함수
+  const fnDelay = async ( t = timed ) => {
+    const promise = new Promise(resolve => {
+      setTimeout( ()=> {
+        permission = true;
+        resolve(permission);
+      }, t)
+    });
+    return await promise;
+  };
+  
+  const fnOpacity = () => {
+    slideDiv[countIndex].style.display = 'block'; // 바로 뒤에 나타나기
+  
+    slideDiv[beforeIndex].style.transition = `all ${timed}ms  ${cssFn}`; // 사라질기능 animation
+    slideDiv[beforeIndex].style.opacity = 0;   // 투명도조절 + 위animation기능 처리
+  
+      indiLi[beforeIndex].classList.remove('act');
+      indiLi[countIndex].classList.add('act');
+  
+      fnDelay(timed)
+        .then( () => {
+          slideDiv[beforeIndex].removeAttribute('style');  // js설정된 style제거 (animate 제거)
+          slideDiv[beforeIndex].classList.remove(CLASS_CHECK); // .act 제거 (display:none)
+          slideDiv[countIndex].classList.add(CLASS_CHECK); // 순번이 바뀐 요소 (.act 첨부)
+          beforeIndex = countIndex;  // 순서 기존변수, 변경변수값 통일
+      });
+  
+    }; // fnOpacity; 
+  // -------------------------------------------------------------
+  // 이벤트처리
+  // elNext 클릭시
+    elNext.addEventListener('click', (e)=>{
+      e.preventDefault();
+      if(permission){
+        permission = false;
+        (countIndex < divLen-1) ? countIndex += 1 : countIndex = 0;
+        fnOpacity();    
+      }
+    });
+  
+    // elPrev 클릭시
+    elPrev.addEventListener('click', (e)=>{
+      e.preventDefault();
+      if(permission){
+        permission = false;
+        (countIndex <= 0) ? countIndex = divLen-1 : countIndex -= 1;
+        fnOpacity(); 
+      }
+    });
+  
+    // indicator 클릭
+    indiLi.forEach((d,i)=>{
+      d.addEventListener('click', (e) => {
+        e.preventDefault();
+        if(permission){
+          permission = false;
+          countIndex = i;
+          fnOpacity(); 
+        }
+      });    
+    })
+  // ----------------------------------------------------------------
+  })() 
