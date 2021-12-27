0a1c981959ab0ca2cc1e841c190b691fc67be113
YEONWOO <89890240+yeonwoochoe@users.noreply.github.com>
2021-12-27 13:18:02 +0900

    [feat] formi :new:  메인페이지 search form box 모바일/피씨 환경에따라 html  js로  컨트롤 구현

---
+++ code/js/src/dom_modify_search_box.js
@@ -0,0 +1,45 @@
+// dom_modify_search_box.js
+
+// 시나리오 : 
+// html 레이아웃 변형
+// 1. 모바일(mobile,tablet)환경일 때 .search_form_box 삭제
+// 2. pc(labtop,pc) 환경일 때 .mo_search_form_box 삭제
+
+(() => {
+
+  // 변수
+  const elMidheaderWrap = document.querySelector('.midheader_wrap');
+  const elMidheader = elMidheaderWrap.querySelector('#mid_header');
+  const elMoNavContainer = elMidheaderWrap.querySelector('.mo_nav_container');
+ 
+
+  const elSearchPc = elMidheader.querySelector('.search_form_box');
+  const elSearchMob = elMoNavContainer.querySelector('.mo_search_form_box');  
+  
+
+
+  
+  // 함수
+  // 처음 실행된 브라우저 너비가 1200보다 작으면 .elSearchPc 제거, 크면 .elSearchMob 제거
+  const fnCkWidth = () => {
+    const browserW = window.matchMedia("all and (max-width: 1199px)").matches;
+
+    if (browserW) {
+      elSearchPc.remove();
+      elMoNavContainer.append(elSearchMob);
+    } else {
+      elSearchMob.remove();
+      elMidheader.append(elSearchPc);
+    }
+  }
+  fnCkWidth();
+  
+  // 이벤트
+  // resize 되면 실행
+  window.addEventListener('resize', () => {
+    fnCkWidth();
+  });
+
+
+
+})();
