e1b57099d13c8b413e7b18d4bd6379661d621e59
yeonwoo <chiyy6162@gmail.com>
2022-05-27 14:57:55 +0900

    comment : 코드 다시 복기

--- code/js/src/import_sub_pd_main_list.js
+++ code/js/src/import_sub_pd_main_list.js
@@ -1,67 +1,41 @@
 // //import_sub_pd_main_list.js
-// // 서브페이지 메인상품리스트 영역 분리하여 불러오기 
+// // 서브페이지 메인상품리스트 영역 분리하여 불러오기
 
 const MainlistPath = "../data/pd_formi_list.json";
 fetch(MainlistPath)
-  .then((response)=> response.json() )
-  .then((response)=>{
+  .then(response => response.json())
+  .then(response => {
     const dataArr = [...response];
     const dataArrLen = dataArr.length;
-    const elMainPdWrap =  document.querySelector('.main_pd_wrap');
+    const elMainPdWrap = document.querySelector(".main_pd_wrap");
 
-
-  //선택자 변수  
-  const MainlistCode = `<h4 class="blind">main_product_name</h4>
+    //선택자 변수
+    const MainlistCode = `<h4 class="blind">main_product_name</h4>
   <div class="thumbnail"><div class="shop_bag_icon"></div>
-</div><div class="description"><div class="main_pd_name"><a href="#">제품명</a><p class="sub_text">서브해시태그</p></div><div class="main_pd_price"><span class="main_pd_p_dc"><span class="blind">할인가</span>24,500원</span><span class="main_pd_per_price"><span class="blind">정가</span>30,500원</span><span class="main_pd_sale_price"><span class="blind">할인률</span>20&#37;</span></div></div>`
+</div><div class="description"><div class="main_pd_name"><a href="#">제품명</a><p class="sub_text">서브해시태그</p></div><div class="main_pd_price"><span class="main_pd_p_dc"><span class="blind">할인가</span>24,500원</span><span class="main_pd_per_price"><span class="blind">정가</span>30,500원</span><span class="main_pd_sale_price"><span class="blind">할인률</span>20&#37;</span></div></div>`;
 
-for( let i =0; i<dataArrLen; i++){
-  const mkDivConBox = document.createElement('div');
-  mkDivConBox.setAttribute('class','main_conbox');
-  mkDivConBox.innerHTML = MainlistCode;
-  elMainPdWrap.append(mkDivConBox);
-}
+    for (let i = 0; i < dataArrLen; i++) {
+      const mkDivConBox = document.createElement("div");
+      mkDivConBox.setAttribute("class", "main_conbox");
+      mkDivConBox.innerHTML = MainlistCode;
+      elMainPdWrap.append(mkDivConBox);
+    }
 
-
-
-function PdDataSet(n){
-  let i = n;
-  const elDivConBox = elMainPdWrap.querySelectorAll('.main_conbox');
-  const elPdImg = elDivConBox[i].querySelector('.thumbnail');
-  const elPdName = elDivConBox[i].querySelector('.main_pd_name a');
-  const elPdSubTxt = elDivConBox[i].querySelector('.sub_text');
-  elPdImg.style.backgroundImage = 'url('+dataArr[i].mainImg+')';
-  elPdName.innerText = dataArr[i].mainPdname;
-  elPdSubTxt.innerText = dataArr[i].contentText;
-}
-dataArr.forEach(function(d,i){
-  PdDataSet(i);
-});
-
-
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    function PdDataSet(n) {
+      let i = n;
+      const elDivConBox = elMainPdWrap.querySelectorAll(".main_conbox");
+      const elPdImg = elDivConBox[i].querySelector(".thumbnail");
+      console.log(i);
+      const elPdName = elDivConBox[i].querySelector(".main_pd_name a");
+      const elPdSubTxt = elDivConBox[i].querySelector(".sub_text");
+      elPdImg.style.backgroundImage = "url(" + dataArr[i].mainImg + ")";
+      elPdName.innerText = dataArr[i].mainPdname;
+      elPdSubTxt.innerText = dataArr[i].contentText;
+    }
+    dataArr.forEach(function (d, i) {
+      PdDataSet(i);
+    });
+  });
 
 // // //data :
 // // let mainData = {
@@ -73,7 +47,6 @@ dataArr.forEach(function(d,i){
 
 // //   {mainTitle : '<h3><span>DAILY HEALTHY MEAL</span>반칙도시락</h3> <h4>맛있는 가벼움, 5가지 건강한 잡곡 데일리 간편도시락 반칙도시락</h4><p>압력밥솥으로 짓는 잡곡밥, 수제반찬으로 언제 어디서나 속 편한 한끼!<br/>다양한 12종 구성으로 남녀노소 일반식으로 적합한 도시락입니다.</p>',
 // //   mainImg: 'rolling_theforme_02.jpg,'},
-
 
 // //   {mainTitle : '<h3><span>DAILY HEALTHY MEAL</span>인생도시락</h3> <h4>맛있는 가벼움, 5가지 건강한 잡곡 데일리 간편도시락 더포르미</h4><p>압력밥솥으로 짓는 잡곡밥, 수제반찬으로 언제 어디서나 속 편한 한끼!<br/>다양한 12종 구성으로 남녀노소 일반식으로 적합한 도시락입니다.</p>',
 // //   mainImg: 'rolling_theforme_03.jpg,'},
@@ -91,22 +64,18 @@ dataArr.forEach(function(d,i){
 // //   {mainPdTitle : '신애한끼 제품보기'}
 // // ],
 
-
 // // mainPdConlist : [
 
 // //   {mainPdTitle : '<h4 class="blind">main_product_name</h4><div class="thumbnail"><div class="shop_bag_icon"></div><img src="../../../img/thumbnail/main_pd_01.jpg" alt="main-product" /></div><div class="description"><div class="main_pd_name"><a href="#">[포르미] 더포르미</a><p class="sub_text">&#35;1주식단 &#35;인기세트 &#35;흑현미3종 &#35;귀리3종 </p></div><div class="main_pd_price"><span class="main_pd_p_dc"><span class="blind">할인가</span>24,500원</span><span class="main_pd_per_price"><span class="blind">정가</span>30,500원</span><span class="main_pd_sale_price"><span class="blind">할인률</span>20&#37;</span>'},
 
 // //   {mainPdTitle : '<h4 class="blind">main_product_name</h4><div class="thumbnail"><div class="shop_bag_icon"></div><img src="../../../img/thumbnail/main_pd_01.jpg" alt="main-product" /></div><div class="description"><div class="main_pd_name"><a href="#">[포르미] 반칙도시락</a><p class="sub_text">&#35;1주식단 &#35;인기세트 &#35;흑현미3종 &#35;귀리3종 </p></div><div class="main_pd_price"><span class="main_pd_p_dc"><span class="blind">할인가</span>24,500원</span><span class="main_pd_per_price"><span class="blind">정가</span>30,500원</span><span class="main_pd_sale_price"><span class="blind">할인률</span>20&#37;</span>'},
 
-
 // //   {mainPdTitle : '<h4 class="blind">main_product_name</h4><div class="thumbnail"><div class="shop_bag_icon"></div><img src="../../../img/thumbnail/main_pd_01.jpg" alt="main-product" /></div><div class="description"><div class="main_pd_name"><a href="#">[포르미] 인생도시락</a><p class="sub_text">&#35;1주식단 &#35;인기세트 &#35;흑현미3종 &#35;귀리3종 </p></div><div class="main_pd_price"><span class="main_pd_p_dc"><span class="blind">할인가</span>24,500원</span><span class="main_pd_per_price"><span class="blind">정가</span>30,500원</span><span class="main_pd_sale_price"><span class="blind">할인률</span>20&#37;</span>'},
-
 
 // //   {mainPdTitle : '<h4 class="blind">main_product_name</h4><div class="thumbnail"><div class="shop_bag_icon"></div><img src="../../../img/thumbnail/main_pd_01.jpg" alt="main-product" /></div><div class="description"><div class="main_pd_name"><a href="#">[포르미] 신애한끼</a><p class="sub_text">&#35;1주식단 &#35;인기세트 &#35;흑현미3종 &#35;귀리3종 </p></div><div class="main_pd_price"><span class="main_pd_p_dc"><span class="blind">할인가</span>24,500원</span><span class="main_pd_per_price"><span class="blind">정가</span>30,500원</span><span class="main_pd_sale_price"><span class="blind">할인률</span>20&#37;</span>'},
 
 // //   ],
 // // };
-
 
 // {
 //   //data 변수
@@ -121,15 +90,12 @@ dataArr.forEach(function(d,i){
 // //   const elMainPdWrap =  elMainPdBox.querySelector('.main_pd_wrap');
 // //   const elMainConbox = elMainPdWrap.querySelector ('.main_conbox');
 
-  
 // //   const elMainPdDescrip =  elMainPdWrap.querySelector('.description');
 
-  
 // // // const card = card[0];
 // // // (elMainConbox)=>{
 // // //   elMainConbox.innerHTML = mainData.mainPdConlist[0].mainPdTitle;
 // // // }
-
 
 // // //생성 후 삽입  =============================================
 
@@ -141,29 +107,12 @@ dataArr.forEach(function(d,i){
 
 // // elMainConbox.innerHTML = mainData.mainPdConlist[0].mainPdTitle;
 
-
-
-
 // // //기능함수 =======================================================
 
-
-// // // 
-
+// // //
 
 //   fetch(subPdListData)
 //     .then( response => response.text() )
 //     .then( (element) =>{elSubPdWrap.innerHTML = element;})
 
-    
-
-
-
-
-
-
-
-    
 //   };
-
-
-
--- code/js/src/rwd_import_header.js
+++ code/js/src/rwd_import_header.js
@@ -1,82 +1,92 @@
 //rwd_import_header.js
 
+{
+  //선택자 변수
+  const elBody = document.querySelector("body");
+  const elHeadWrap = document.querySelector(".head_wrapper");
 
+  // ======================================================
+  // 3가지 디바이스 환경 반응형 변수  :size오타
+  const mobileSize = `screen and (max-width:767px)`;
+  const tabSize = `screen and (min-width:768px) and (max-width:1199px)`;
+  const pcSize = `screen and (min-width:1200px)`;
 
-{  
-  
-//선택자 변수
-const elBody = document.querySelector('body');
-const elHeadWrap = document.querySelector('.head_wrapper');
+  let nowDevice;
 
+  //============================================================
+  //3가지 디바이스 환경 html, js 객체화 변수
 
-// ======================================================
-// 3가지 디바이스 환경 반응형 변수  :size오타
-const mobileSize = `screen and (max-width:767px)`;
-const tabSize = `screen and (min-width:768px) and (max-width:1199px)`;
-const pcSize = `screen and (min-width:1200px)`;
+  const deviceCheck = [
+    {
+      type: "mobile",
+      check: mobileSize,
+      code: "../html/temp/mob_header.html",
+      script: "../js/src/rwd/mobile_header.js",
+    },
+    {
+      type: "tablet",
+      check: tabSize,
+      code: "../html/temp/tablet_header.html",
+      script: "../js/src/rwd/tablet_header.js",
+    },
+    {
+      type: "pc",
+      check: pcSize,
+      code: "../html/temp/pc_header.html",
+      script: "../js/src/rwd/pc_header.js",
+    },
+  ];
 
-let nowDevice;
-
-//============================================================
-//3가지 디바이스 환경 html, js 객체화 변수 
-
-const deviceCheck = [
-  {type:'mobile', check:mobileSize , code: '../html/temp/mob_header.html', script:'../js/src/rwd/mobile_header.js'},
-  {type:'tablet', check:tabSize , code: '../html/temp/tablet_header.html', script:'../js/src/rwd/tablet_header.js'},
-  {type:'pc', check:pcSize , code: '../html/temp/pc_header.html', script:'../js/src/rwd/pc_header.js'}
-];
-
-//기능 함수
-const fnMKScript = (data)=>{
-  // .navScript 요소가 존재한다면 선택 후 삭제 (기존 script문서 삭제)
-  const ckScript = document.querySelector('.navScript');
-  if(!!ckScript){ckScript.remove();} 
-  // 새로운 기능을 가진 script를 생성 및 첨부
-  const mkScript = document.createElement('script');
-  mkScript.setAttribute('src', data);
-  mkScript.setAttribute('class', 'navScript');
-  elBody.append(mkScript);
-};
-
-//여기까지는 이해감..그이후로 해석불가능,,
-//-----------------------------------------------------------
-
-
-const fnDevice = (num) =>{
-  const deviceN = deviceCheck[num];
-  fetch(deviceN.code)
-  .then(response => response.text()) // html 불러오기
-  .then(data  => elHeadWrap.innerHTML = data) // 불러온 html삽입
-  .then(() => fnMKScript(deviceN.script)) // html형식에 맞는 script코드 삽입  // code오타
+  //기능 함수
+  const fnMKScript = data => {
+    // .navScript 요소가 존재한다면 선택 후 삭제 (기존 script문서 삭제)
+    const ckScript = document.querySelector(".navScript");
+    if (!!ckScript) {
+      ckScript.remove();
+    }
+    // 새로운 기능을 가진 script를 생성 및 첨부
+    const mkScript = document.createElement("script");
+    mkScript.setAttribute("src", data);
+    mkScript.setAttribute("class", "navScript");
+    elBody.append(mkScript);
   };
 
+  //여기까지는 이해감..그이후로 해석불가능,,
+  //-----------------------------------------------------------
 
-// 기본화면에서 체크 : 사이즈변화이전 기본형태에서 체크 미작성
-  deviceCheck.forEach ((media,index)=>{  // 배열형식으로된내용 각각 수행하여 체크
-  const mediaQuery = window.matchMedia(media.check);  // type-insert    
-    if(mediaQuery.matches){ 
-      nowDevice =  deviceCheck[index].type; 
+  const fnDevice = num => {
+    const deviceN = deviceCheck[num];
+    console.log(num);
+    fetch(deviceN.code)
+      .then(response => response.text()) // html 불러오기
+      .then(data => (elHeadWrap.innerHTML = data)) // 불러온 html삽입
+      .then(() => fnMKScript(deviceN.script)); // html형식에 맞는 script코드 삽입  // code오타
+  };
+
+  // 기본화면에서 체크 : 사이즈변화이전 기본형태에서 체크 미작성
+  deviceCheck.forEach((media, index) => {
+    // 배열형식으로된내용 각각 수행하여 체크
+    const mediaQuery = window.matchMedia(media.check); // type-insert
+    //MediaQueryList 속성이용하여 불린데이터 반환
+    if (mediaQuery.matches) {
+      nowDevice = deviceCheck[index].type;
       console.log(nowDevice);
       fnDevice(index);
     }
   });
-  
-// 페이지 사이트 변화시 체크
-deviceCheck.forEach ((media,index)=>{  // 배열형식으로된내용 각각 수행하여 체크
-  const mediaQuery = window.matchMedia(media.check);  // type-insert
-  mediaQuery.addEventListener('change',(e)=>{ // mediaQuery 변화시 체크
-    if(e.matches){ 
-      nowDevice =  deviceCheck[index].type; 
-      console.log(nowDevice);
-      fnDevice(index);
-    }
+
+  // 페이지 사이트 변화시 체크
+  deviceCheck.forEach((media, index) => {
+    // 배열형식으로된내용 각각 수행하여 체크
+    //matchMedia 미디어 쿼리를 나타내는 문자열.
+    const mediaQuery = window.matchMedia(media.check); // type-insert
+    mediaQuery.addEventListener("change", e => {
+      // mediaQuery 변화시 체크
+      if (e.matches) {
+        nowDevice = deviceCheck[index].type;
+        console.log(nowDevice);
+        fnDevice(index);
+      }
+    });
   });
-});
-
-
-
-
-
 }
-
-
